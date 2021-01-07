87abd6f5f39b9e467936e8c9a2dc0db68ac2dacc
luisvinicius09 <luisvinicius0906@gmail.com>
2021-01-07 17:32:44 -0300

    Delete useless file

--- test_samples.rb
+++
@@ -1,4 +0,0 @@
-# This is a test sample for the method above, just remove the hashtag below & run it through the console.
-# [1 ,2, 3, 4, 5, 6, 7].my_each {|element| puts "hello #{element}"}
-
-# [6 ,4, 3, 2, 98, 53, 7].my_each_with_index {|element, index| puts "Hello number #{index}, you are #{element}"}
