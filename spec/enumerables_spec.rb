404f1b855c3e3964a8740d684e4e7005f30432ea
GuNner-pete <72259344+GuNner-pete@users.noreply.github.com>
2021-01-27 21:41:37 +0200

    More tests for methods were added

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -44,4 +44,124 @@ describe Enumerable do
       expect(array.my_any?(String)).to eq(array.any?(String))
     end
   end
+
+  #my_none? -- false_array
+  describe '#my_none?' do
+    it 'return true' do
+      expect(false_array.my_none?(Numeric)).to eq(false_array.none?(Numeric))
+    end
+  end
+
+  #my_none? -- array
+  describe '#my_none?' do
+    it 'return false'do
+      expect(array.my_none?(String)).to eq(array.none?(String))
+    end
+  end
+
+  # my_select -- array
+  describe '#my_select' do
+    it 'my_select should return even array' do
+      expect(array.my_select(&:even?)).to eq(array.select(&:even?))
+    end
+  end
+
+  # my_select -- range
+  describe '#my_select' do
+    it 'my_select should return even array from range' do
+      expect(range.select(&:even?)).to eq(range.select(&:even?))
+    end
+  end
+
+  # my_all -- array
+  describe '#my_all?' do
+    it 'my_all should return false' do
+      expect(array.my_all?(&:even?)).to eql(array.all?(&:even?))
+    end
+  end
+
+  # my_all without block or args
+  describe '#my_all?' do
+    it 'my_all should return false' do
+      expect([nil, true, 99].my_all?).to eq([nil, true, 99].all?)
+    end
+  end
+
+  # my_all -- Class
+  describe '#my_all?' do
+    it 'my_all should return true' do
+      expect(range.my_all?(Numeric)).to eql(range.all?(Numeric))
+    end
+  end
+
+  # my_all -- Pattern
+  describe '#my_all?' do
+    it 'my_all should return false' do
+      expect(%w[ant bear cat].my_all?(/t/)).to eql(%w[ant bear cat].all?(/t/))
+    end
+  end
+
+  # my_map_with_block
+  describe '#my_map' do
+    it 'my_map should return the same as map' do
+      expect(array.my_map { |element| element * 5 }).to eq(array.map { |element| element * 5 })
+    end
+  end
+
+  # my_map_with_Proc
+  describe '#my_map' do
+    it 'my_map should return the same as map' do
+      expect(array.my_map(&procc)).to eq(array.map(&procc))
+    end
+  end
+
+  # my_inject
+  describe '#my_inject' do
+    it 'my_inject should return the same Total of a sum' do
+      expect(array.my_inject { |total, element| total + element }).to eq(array.inject { |total, element| total + element})
+    end
+  end
+
+  # my_inject  -- Block and initial value
+  describe '#my_inject' do
+    it 'my_inject should return the same Total of a multiplication' do
+      expect((5..10).my_inject(1) { |product, n| product * n }).to eq((5..10).inject(1) { |product, n| product * n })
+    end
+  end
+
+  # my_inject  --  Symbol
+  describe '#my_inject' do
+    it 'my_inject should return the same Total of a multiplication' do
+      expect((5..10).my_inject(:*)).to eq((5..10).inject(:*))
+    end
+  end
+
+  # my_inject  --  initial value and symbol
+  describe '#my_inject' do
+    it 'my_inject should return the same Total of a sum' do
+      expect((5..10).my_inject(12, :+)).to eq((5..10).inject(12, :+))
+    end
+  end
+
+  # my_count
+  describe '#my_count' do
+    it 'my_count should be equal to count' do
+      expect(array.my_count).to eq(array.count)
+    end
+  end
+
+  # my_count --arg
+  describe '#my_count' do
+    it 'my_count should be equal to count' do
+      arr = [1, 2, 2, 2, 3]
+      expect(arr.my_count(2)).to eq(arr.count(2))
+    end
+  end
+
+  # my_count -- Block
+  describe '#my_count' do
+    it 'my_count should be equal to count' do
+      expect(array.my_count(&:even?)).to eq(array.count(&:even?))
+    end
+  end
 end
